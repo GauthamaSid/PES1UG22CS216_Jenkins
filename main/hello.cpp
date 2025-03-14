6bfcd6e7215d6a7f7dba4eff1048e08f6a2bcad4
Gauthama Siddharth <gauthama.siddharth@gmail.com>
2025-03-14 17:37:38 +0000

    Added hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "He World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this now, idkc  \n";
   return 0;
